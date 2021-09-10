f6d25a006aa724f39090df639879c6440a85ef9c
EC2 Default User <mei013h0ws@gmail.com>
2021-09-10 12:28:34 +0000

    channnel delete

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,22 +1,22 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
-    identified_by :current_user
+    #identified_by :current_user
     #channnelでcurrent_userが使えるようにする
 
-    def connect
-      self.current_user = find_verified_user
-    end
+    #def connect
+      #self.current_user = find_verified_user
+    #end
 
-    protected
-    def find_verified_user
-      verified_user = User.find_by(id: env['warden'].user.id)
-      return reject_unauthorized_connection unless verified_user
-      verified_user
-    end
+    #protected
+    #def find_verified_user
+     # verified_user = User.find_by(id: env['warden'].user.id)
+      #return reject_unauthorized_connection unless verified_user
+      #verified_user
+    #end
 
-    def session
-      cokkies.encrypted[Rails.application.config.session_options[:key]]
-    end
+    #def session
+     # cokkies.encrypted[Rails.application.config.session_options[:key]]
+    #end
 
   end
 end
