1f7ee5f86571dafed2fba06496e72f91e4ac5813
EC2 Default User <mei013h0ws@gmail.com>
2021-10-15 08:36:27 +0000

    通知機能　controleller変更中

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,27 +1,27 @@
 class ChatsController < ApplicationController
  before_action :authenticate_user!
 
- #def index
-  #@users = User.all.page(params[:page]).per(10)
- #end
+ def index
+  @users = User.all.page(params[:page]).per(10)
+ end
 
- #def show
-  #@user = User.find(params[:id])
-  #rooms = current_user.user_rooms.pluck(:room_id)
-  #user_rooms = UserRoom.find_by(user_id: @user.id, room_id: rooms)
+ def show
+  @user = User.find(params[:id])
+  rooms = current_user.user_rooms.pluck(:room_id)
+  user_rooms = UserRoom.find_by(user_id: @user.id, room_id: rooms)
 
-  #if user_rooms.nil? #チャットroomがない場合新規作成
-   #@room = Room.new
-   #@room.save
-   #UserRoom.create(user_id: @user.id, room_id: @room.id) #チャット相手のroom作成
-   #UserRoom.create(user_id: current_user.id, room_id: @room.id) #カレントユーザーのroom作成
-  #else #チャットルームがすでにある場合の分岐
-   #@room = user_rooms.room
-  #end
+  if user_rooms.nil? #チャットroomがない場合新規作成
+   @room = Room.new
+   @room.save
+   UserRoom.create(user_id: @user.id, room_id: @room.id) #チャット相手のroom作成
+   UserRoom.create(user_id: current_user.id, room_id: @room.id) #カレントユーザーのroom作成
+  else #チャットルームがすでにある場合の分岐
+   @room = user_rooms.room
+  end
 
-  #@chats = @room.chats
-  #@chat = Chat.new(room_id: @room.id)
- #end
+  @chats = @room.chats
+  @chat = Chat.new(room_id: @room.id)
+ end
 
  def create
   @chat = current_user.chats.new(chat_params)
